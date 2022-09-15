02bdb87b70b15cd8284f371dc4fdabf8f7361787
harbaz3301 <harbazbrar3223@gmail.com>
2022-09-15 11:40:47 +0530

    cascasc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') localchild!: NgForm;
   title = 'testproject';
+  btn = '';
+  signupform!: FormGroup;
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  // }
+  constructor(private http: HttpClient) { }
+  ngOnInit(){
+    this.signupform = new FormGroup({
+      'email': new FormControl(null, Validators.required),
+      'password': new FormControl()
+    });
+  }
+  suggest(){
+    this.localchild.form.patchValue({
+      userData: {
+        email: 'dasdasdasdasd'
+      }
+    });
+  }
+  onSubmit(){
+    console.log(this.signupform.controls['email'].value);
+    // this.http.get()
+  }
 }
